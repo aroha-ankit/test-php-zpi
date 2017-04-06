1022a6faa2e7ff9e09d88ee66fb5d79fb2225c41
Ankit <Ankit>
2017-04-06 18:20:24 +0530

    indexva

---
+++ index.php
@@ -0,0 +1,6 @@
+<?php
+header('Content-type: application/json');
+echo  '{"speech" : "Barack Hussein Obama II is the 44th and current President of the United States.",
+"displayText" : "Barack Hussein Obama II is the 44th and current President of the United States, and the first African American to hold the office. Born in Honolulu, Hawaii, Obama is a graduate of Columbia University   and Harvard Law School, where ",
+"source" : "DuckDuckGo"}';
+?>
